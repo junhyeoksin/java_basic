f2134a8dfd34837e4907e0e89be7a1130ccc6d66
junhyeoksin <92718453+junhyeoksin@users.noreply.github.com>
2022-04-15 10:07:12 +0900

    first commit

---
+++ src/com/basic/java/Main.java
@@ -0,0 +1,9 @@
+package com.basic.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("start ");
+    }
+}
---
+++ src/com/basic/java/stream.java
@@ -0,0 +1,41 @@
+package com.basic.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class stream {
+    public static void main(String[] args) {
+
+        /**
+         * list -> stream
+         */
+        System.out.println("-------------list -> stream---------------------");
+//      List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list1 = Arrays.asList(2, 1, 30, 3, 4, 10, 7);
+        Stream<Integer> intStream = list1.stream().sorted();
+        intStream.forEach(System.out::println);
+
+
+        /**
+         *  String 배열 -> Stream
+         */
+        System.out.println("-------------String 배열 -> Stream---------------------");
+        String [] strArr = new String[] {"a", "b", "c"};
+        Stream<String> strStream = Arrays.stream(strArr);
+        strStream.forEach(System.out::println);
+
+        /**
+         * integer -> stream
+         */
+        System.out.println("-------------integer -> stream---------------------");
+        Integer[] intArr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8};
+        Stream<Integer> intStream2 = Arrays.stream(intArr);
+        //intStream2.forEach(System.out::println);
+        System.out.println("intStream2 = " +  intStream2.count());
+
+
+    }
+}
